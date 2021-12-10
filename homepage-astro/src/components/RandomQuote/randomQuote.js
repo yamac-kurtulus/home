54bf81eb7cf3559d910ac6cb5b202cba9e50d951
Yamac <yamac@ceng.metu.edu.tr>
2021-12-10 03:07:58 +0300

    Some progress

--- homepage-astro/src/components/RandomQuote/randomQuote.js
+++
@@ -1,6 +0,0 @@
-export const logFileText = async file => 
-{
-    const response = await fetch(file)
-    const text = await response.text()
-    console.log(text)
-}
--- homepage-astro/tailwind.config.js
+++ homepage-astro/tailwind.config.js
@@ -2,11 +2,10 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
     mode: 'jit',
-    purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
-    darkMode: true, // or 'media' or 'class'
-    // theme: {
-    //   extend: {},
-    // },
+    purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx, vue}'],
+    darkMode: true,
     variants: {},
-    plugins: [],
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
   }
