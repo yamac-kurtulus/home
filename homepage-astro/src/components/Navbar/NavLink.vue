21f0836ab3f4a4106fbdc4f8dff0409f33010350
Yamac <yamac@ceng.metu.edu.tr>
2021-12-14 01:34:06 +0300

    Navbar modularization

---
+++ homepage-astro/src/components/Navbar/NavLink.vue
@@ -0,0 +1,48 @@
+<template>
+  <button>
+    <button class="hover:scale-125 transform-gpu transition-all">
+      <a :href="href" :class="getActiveState()">{{ label }}</a>
+    </button>
+  </button>
+</template>
+
+<style scoped>
+.active {
+  @apply text-green-500 font-bold text-lg hover:scale-125;
+}
+.inactive {
+  @apply text-pink-600 text-lg hover:scale-125;
+}
+</style>
+
+<script>
+export default {
+  name: 'NavLink',
+  props: {
+    active: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    href: {
+      type: String,
+      default: '#',
+      required: false,
+    },
+    label: {
+      type: String,
+      default: '#',
+      required: false,
+    },
+  },
+  setup(props) {
+    function getActiveState() {
+      return props.active ? 'active' : 'inactive'
+    }
+
+    return { getActiveState }
+  },
+}
+</script>
+
+
---
+++ homepage-astro/src/components/Navbar/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav>
+    <div class="container m-auto">
+      <div class="flex">
+        <div id="homelink" class="container flex flex-initial w-1/2 m-4 mb-2">
+          <NavLink href="/" label="Home" :active="isActive('\/')" />
+        </div>
+        <!--mobile menu button-->
+        <div class="md:hidden flex-row-reverse w-1/2 row-end-auto m-4 mb-2 text-pink-600">
+          <button class="md:hidden flex flex-row-reverse" @click="showTopMenu = !showTopMenu">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7" viewBox="0 0 20 20" fill="currentColor">
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <!--Desktop Menu-->
+        <div class="md:flex-row-reverse md:w-1/2 md:row-end-auto md:flex hidden">
+          <ul id="pagelinks-desktop" class="flex flex-row">
+            <li v-for="item in links" :key="item.href" class="md:m-8 flex">
+              <div class="x">
+                <NavLink :href="item.href" :label="item.label" :active="isActive(item.href)" />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--Mobile Menu Items-->
+      <div v-show="showTopMenu">
+        <ul id="pagelinks-mobile" class="flex flex-row">
+          <li v-for="item in links" :key="item.href" class="md:object-right flex-auto text-center">
+            <div class="">
+              <NavLink :href="item.href" :label="item.label" :active="isActive(item.href)" />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+</style>
+
+<script>
+import NavLink from './NavLink.vue'
+export default {
+  data() {
+    return {
+      links: [
+        { href: '/projects', label: 'Projects' },
+        { href: '/blog', label: 'Blog' },
+        { href: '/about', label: 'About' },
+        { href: '/contact', label: 'Contact' },
+      ],
+      showTopMenu: false,
+    }
+  },
+  props: ['currentPage'],
+  methods: {
+    isActive(url) {
+      const section = url.split('/')[1]
+      if (section === this.currentPage) {
+        return true
+      } else {
+        return false
+      }
+    },
+  },
+  components: {
+    NavLink,
+  },
+}
+</script>
