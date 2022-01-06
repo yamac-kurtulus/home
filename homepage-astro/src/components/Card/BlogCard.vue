47be3c50ff1b9b9bfb21936b3b2acbf6cb77fa2f
Yamac <yamac@ceng.metu.edu.tr>
2022-01-06 11:36:34 +0300

    Improve blog posts and add utterances

--- homepage-astro/src/components/Card/BlogCard.vue
+++ homepage-astro/src/components/Card/BlogCard.vue
@@ -2,13 +2,15 @@
   <div
     class="relative h-64 overflow-hidden transition-transform rounded-lg shadow-lg  shadow-main-purple hover:-translate-y-1"
   >
-    <img class="justify-center object-scale-down" :src="item.heroImg" :alt="item.alt" />
+    <!-- Notice alt="" because hero images are merely for decoration -->
+
+    <img class="relative justify-center object-cover w-full h-full aspect-video" :src="item.heroImg" alt="" />
     <a :href="item.url" class="">
-      <span class="absolute top-0 block h-full px-6 py-4 bg-gray-900 bg-opacity-80">
+      <span class="absolute top-0 block w-full h-full px-6 py-4 bg-gray-900 bg-opacity-80">
         <p class="pt-2 pb-6 text-2xl strong">{{ item.title }}</p>
-        <p class="pt-2 text-base text-main-light-blue">{{ item.summary }}</p>
+        <p class="pt-2 text-base text-main-light-blue">{{ item.metaDescription }}</p>
         <p class="pt-2 text-base italic text-right text-main-light-blue">
-          {{ format(item.date, 'dd/MM/YYY') }}
+          {{ format(new Date(item.date), 'dd/MM/YYY') }}
         </p>
       </span>
     </a>
@@ -16,7 +18,6 @@
 </template>
 <script>
 import { format } from 'date-fns'
-
 export default {
   props: {
     item: {
